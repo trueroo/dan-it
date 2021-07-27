08c442a54deb8ff2c6aef53a15d640058fa93ee0
Eugene Alforov <eugenalforov@gmail.com>
2021-07-27 23:23:43 +0300

    classwork 8-10

---
+++ programming-essentials/basic-javascript/classwork/classwork8/script.js
@@ -0,0 +1,25 @@
+// const numbers = [4, 2, 7, 1, 8];
+
+// let firstElementBiggerThenSix;
+
+// for (let number of numbers) {
+//   if (number > 6) {
+//     firstElementBiggerThenSix = number;
+//     break;
+//   }
+// }
+
+// let firstElementBiggerThenSix1 = 
+//   numbers.find(number => number > 6);
+
+
+
+// let doubledNumbers = [];
+// for (let number of numbers) {
+//   doubledNumbers.push(number * 2);
+// }
+
+// let doubledNumbers1 = numbers.map(number => number * 2);
+
+// console.log(doubledNumbers1);
+
